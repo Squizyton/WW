13f9b1beb8507138a5e709ff2a0bc88e84fc2b44
Squizyton <lordmlgpro@gmail.com>
2017-05-24 00:09:41 -0400

    Added Basic Character Movement Script
    This was a pain in the ass.

---
+++ Assets/Scripts/Character_Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour 
+
+{
+    public float speed = 10, jumpVelocity = 10;
+    public LayerMask playerMask;
+    public bool canMoveInAir = true;
+    Transform myTrans, tagGround;
+    Rigidbody2D myBody;
+    bool isGrounded = false;
+    float hInput = 0;
+
+    void Start()
+    {
+    
+        myBody = this.GetComponent<Rigidbody2D>();//Unity 5+
+        myTrans = this.transform;
+ 
+    }
+
+    void FixedUpdate()
+    { 
+
+
+        Move(Input.GetAxisRaw("Horizontal"));
+        if (Input.GetButtonDown("Space"))
+        {
+            Jump(); 
+        }
+    }
+
+    void Move(float horizonalInput)
+    {
+ 
+        Vector2 moveVel = myBody.velocity;
+        moveVel.x = horizonalInput * speed;
+        myBody.velocity = moveVel;
+    }
+
+    public void Jump()
+    {
+        myBody.velocity += jumpVelocity * Vector2.up;
+    }
+
+
+}
