d7faeece2fd2ad047569e1e1d1c957fed00019ab
JayWolvian <jaywolvian@gmail.com>
2017-05-25 13:32:36 -0400

    Boom go away
    Boom, then no boom

---
+++ Assets/Scripts/DeadBoom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadBoom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(killBoom ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	IEnumerator killBoom(){
+		yield return new WaitForSeconds(2.0f);
+		Destroy (this.gameObject);
+	}
+}
