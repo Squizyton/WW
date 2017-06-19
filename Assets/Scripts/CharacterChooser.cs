ba7d3b03c967e3c06b6266e777d8568bb8b71eda
Squizyton <lordmlgpro@gmail.com>
2017-06-18 23:34:23 -0400

    CHARACTER CHOOSING HAS BEEN IMPLEMENTED!
    YES YOU READ THE TITLE RIGHT}?!
    
    It is bug free entirely.
    
    And it fully works as intended.
    
    Of course it needs to be edited for actual characters and such.
    
    But it is there :)

---
+++ Assets/Scripts/CharacterChooser.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CharacterChooser : MonoBehaviour {
+
+    public bool HasPicked = false;
+
+
+
+    public const int Bob = 2;
+    public const int Larry = 3;
+    public const int Harold = 4;
+    public const int Jim  = 5;
+
+    public static int CharacterSelected = 0;
+
+
+
+    // Use this for initialization
+    void OnGUI() {
+
+
+
+        if (GUI.Button(new Rect(Screen.width * .30f, Screen.height * .35f, 100, 100), "Larry"))
+        {
+
+            CharacterSelected = 3;
+            HasPicked = true;
+
+        }
+        if (GUI.Button(new Rect(Screen.width * .40f, Screen.height * .35f, 100, 100), "Harold"))
+        {
+            CharacterSelected = 4;
+            HasPicked = true;
+
+        }
+
+        if (GUI.Button(new Rect(Screen.width * .50f, Screen.height * .35f, 100, 100), "Jim"))
+
+        {
+            CharacterSelected = 5;
+            HasPicked = true;
+
+        }
+
+        if (GUI.Button(new Rect(Screen.width * .60f, Screen.height * .35f, 100, 100), "Bob"))
+
+        {
+
+            CharacterSelected = 2;
+            HasPicked = true;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (HasPicked == true)
+        {
+
+            SceneManager.LoadScene("Test Scene");
+        
+
+
+        }
+
+
+
+	}
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+}
---
+++ Assets/WhatCharacterWasChosen.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatCharacterWasChosen : MonoBehaviour {
+
+    public Transform Bobby,Harold2,Jim2,Larry2, SpawnPoint;
+
+    public bool Spawned = false;
+
+	void Update () {
+
+
+        switch (CharacterChooser.CharacterSelected)
+        {
+            case 2:
+                Bob();
+                break;
+            case 3:
+                Larry();
+                break;
+            case 4:
+               Harold();
+                break;
+            case 5:
+               Jim();
+                break;
+        }
+
+
+
+	}
+
+    void Bob()
+    {
+        if (Spawned == false)
+        { 
+        Instantiate(Bobby, SpawnPoint.position, SpawnPoint.rotation);
+        Destroy(GameObject.Find("Character Chooser"));
+        Spawned = true;
+    }
+
+    }
+
+    void Larry()
+
+    {
+        {
+            if (Spawned == false)
+                Instantiate(Larry2, SpawnPoint.position, SpawnPoint.rotation);
+            Destroy(GameObject.Find("Character Chooser"));
+            Spawned = true;
+        }
+
+        }
+    void Jim()
+
+    {
+        if (Spawned == false)
+        { 
+            Instantiate(Jim2, SpawnPoint.position, SpawnPoint.rotation);
+        Destroy(GameObject.Find("Character Chooser"));
+        Spawned = true;
+    }
+    }
+    void Harold()
+
+    {
+        if (Spawned == false)
+        {
+            Instantiate(Harold2, SpawnPoint.position, SpawnPoint.rotation);
+            Destroy(GameObject.Find("Character Chooser"));
+            Spawned = true;
+        }
+        }
+}
