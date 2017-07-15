d14c13c45709ba7e026a1671ea2338ddd32d59be
Squizyton <lordmlgpro@gmail.com>
2017-07-15 18:20:31 -0400

    Clean Up + next steps!
    Heyo! Thanks for the recent update.
    
    Really didn't do much this time around. Bug fixing, did some much needed folder and such clean up.
    
    So, I need a list of characters and their respective spells so I can start working on those.
    
    I have the list of characters we suggested and if you need that I can send it too you.
    
    Some sort of animations or something (even if they are stick figures) would be absolutely wonderful.
    
    Other then that I will probably pick away at some minor things until you figure out the next major thing you want me to do!

--- Assets/Scripts/Regular Scripts/MenuGUI.cs
+++ Assets/Scripts/Regular Scripts/MenuGUI.cs
@@ -5,18 +5,96 @@ using UnityEngine;
 
 public class MenuGUI : MonoBehaviour {
 
+    public bool enter_the_code = false;
+
+
+    public string stringToEdit = "Enter The Code <3";
+
+    public const int StartGUI = 1;
+    public const int OPTIONS = 2;
+    public const int EXTRAS = 3;
+
+    public static int Selection = StartGUI;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
 
-	void OnGUI(){
-		GUI.Box (new Rect (Screen.width * 0.2f, Screen.height * 0.01f, Screen.width * 0.6f, Screen.height * 0.1f), "MAIN TITLE");
+
+
+    	}
+
+
+
+    void OPTIONSMENU()
+    {
+
+
+        //Things Go Here. Mostly Stuff and things Carl.
+
+
+    }
+
+
+
+    void EXTRASMENU()
+    {
+
+        if (GUI.Button(new Rect(Screen.width * 0.3f, Screen.height * 0.21f, Screen.width * 0.4f, Screen.height * 0.1f), "ENTER Code"))
+
+        {
+            stringToEdit = GUI.TextField(new Rect(Screen.width * 0.3f, Screen.height * 0.30f, Screen.width * 0.4f, Screen.height * 0.1f), stringToEdit, 25);
+
+            if (stringToEdit == "UPUPUPDOWNDOWNLEFTRIGHTLEFTRIGHTSTARTSELECT")
+            {
+
+               
+
+            }
+
+        }
+
+
+
+
+
+        if (GUI.Button(new Rect(Screen.width * 0.3f, Screen.height * 0.45f, Screen.width * 0.4f, Screen.height * 0.1f), "Back"))
+        {
+
+            Selection = StartGUI;
+
+        }
+
+    }
+    void OnGUI()
+    {
+        GUI.Box(new Rect(Screen.width * 0.2f, Screen.height * 0.01f, Screen.width * 0.6f, Screen.height * 0.1f), "MAIN TITLE");
+        switch (Selection)
+        {
+            case OPTIONS:
+                OPTIONSMENU();
+                break;
+
+            case EXTRAS:
+                EXTRASMENU();
+                break;
+
+            case StartGUI:
+                StartMENU();
+                break;
+        }
+
+    }
+        void StartMENU()
+        { 
+
+      
 		if(GUI.Button(new Rect(Screen.width*0.3f,Screen.height*0.21f, Screen.width*0.4f,Screen.height*0.1f),"ARCADE MODE")){
 		}
 		if(GUI.Button(new Rect(Screen.width*0.3f,Screen.height*0.31f, Screen.width*0.4f,Screen.height*0.1f),"STORY MODE")){
@@ -29,8 +107,22 @@ public class MenuGUI : MonoBehaviour {
 		if(GUI.Button(new Rect(Screen.width*0.3f,Screen.height*0.61f, Screen.width*0.4f,Screen.height*0.1f),"OPTIONS")){
 			
 		}
-		if(GUI.Button(new Rect(Screen.width*0.3f,Screen.height*0.71f, Screen.width*0.4f,Screen.height*0.1f),"EXTRAS")){ //If any
-		}
+		if(GUI.Button(new Rect(Screen.width*0.3f,Screen.height*0.71f, Screen.width*0.4f,Screen.height*0.1f),"EXTRAS")){
+
+
+
+
+            Selection = EXTRAS;
+
+
+
+
+        }
 			
 	}
+
+    private void NewMethod()
+    {
+        OnGUI();
+    }
 }
